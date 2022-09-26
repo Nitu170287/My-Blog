286bac84bb23c1e483b8b9bdad6c0d1304a92e47
Nitu Dubey <nitutiwari01@gmail.com>
2022-09-26 15:06:54 -0400

    first commit

---
+++ BlogServer/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+
+const app = express();
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+
+const mongoUri = "mongodb+srv://blog123:blog321@cluster0.w3xmqjt.mongodb.net/myBlog?retryWrites=true&w=majority"
+
+mongoose.connect(mongoUri)
+app.use(bodyParser.json())
+
+////Schema///
+const blogSchema = mongoose.Schema({
+    id:Number,
+    blog:String
+})
+
+const blog = mongoose.model("blog",blogSchema )
+
+//////////////////////////////////////////////
+app.get("/api/getblog/:id",(req,res)=>{
+    blog.find({id:req.params.id},(err,doc)=>{
+        if(err) return console.log(err)
+        res.json(doc)
+    })
+})
+
+app.get("/api/getblogs",(req,res)=>{
+    blog.find((err,doc)=>{
+        if(err) return console.log(err)
+        res.json(doc)
+    })
+})
+
+
+
+app.post("/api/addblog",(req,res)=>{
+    const addblog = new blog({
+        id:req.body.id,
+        blog:req.body.blog
+
+    })
+    addblog.save((err,doc)=>{
+        if(err)return console.log(err)
+        res.json("successfully posted blog")
+    })
+    })
+
+    
+
+
+
+
+const port = process.env.POST || 4000
+app.listen(port)
